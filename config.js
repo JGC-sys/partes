df274647977cf01d23e72b53db2b1bce26c9465b
JGC-sys <jguerrerocarv@gmail.com>
2026-02-09 08:30:25 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// =================================== 
+// ARCHIVO DE CONFIGURACIÓN
+// =================================== 
+
+// ⬇️ CAMBIA SOLO ESTA URL CUANDO ACTUALICES EL DESPLIEGUE
+const CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycby6MoZWCksJjf7Vq4BO7h8jFd5QhswBeRfFt1UzELrFLc5eVWfjHAdhZVV2t0vJEksU4w/exec'
+};
+
+// No modificar nada más abajo
+window.CONFIG = CONFIG;
