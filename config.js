8b2643de7e4f5b6507c011a0f36564e058f98366
JGC-sys <jguerrerocarv@gmail.com>
2026-02-09 09:40:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 // ⬇️ CAMBIA SOLO ESTA URL CUANDO ACTUALICES EL DESPLIEGUE
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbylCRE1bkCoPSpBX6kIMdHLukrVCuYDXGlpeU1UNN3r4eJxg7KSyqG25I7w_gW6N0l40g/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbxq-WmWtqlVrYroNHDLqaIZakIsVHWWvCj0sEiWiCQoBRY3vSqC8ju2Z5geoIHXhfwqEQ/exec'
 };
 
 // No modificar nada más abajo
