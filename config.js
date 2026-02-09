72ffdc5747f9936362ec97748931770d47561317
JGC-sys <jguerrerocarv@gmail.com>
2026-02-09 12:04:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 // ⬇️ CAMBIA SOLO ESTA URL CUANDO ACTUALICES EL DESPLIEGUE
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbzPGqnOAxvJMcpI-QkeWmG1AlbJQLNBfdOxzcxEe3Dc7C_5UYiTubQppmYvDjqzce42/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbw6ydYtbRKttFC5axDc1blQodkkfOKjRK9OSv8hXk37OUwmwwXa1V9F-D65ppr4mbChHg/exec',
     API_TOKEN: 'PARTES_' + new Date().getFullYear() + '_' + (new Date().getMonth() + 1)
 };
 window.CONFIG = CONFIG;
